ba993adc3c03d1b0b0aa89a1f933283c1524895e
Samuel Mbah <samuelmbah24@gmail.com>
2023-09-07 07:16:36 -0700

    Puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include <stdio.h>
 /**
  * main - this is the main put function
@@ -6,6 +5,6 @@
  */
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-                return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
