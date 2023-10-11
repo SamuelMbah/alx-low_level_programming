dab077e576caef712c9abaacc3ca32f0ba8967fd
SamuelMbah <samuelmbah24@gmail.com>
2023-10-11 00:44:38 -0700

    new struct dog using typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,18 +9,17 @@
  * Description: Longer description
  */
 
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
 /**
  * dog_t - typedef for struct dog
  */
-
 typedef struct dog dog_t;
-
-struct dog
-{
-char *name;
-float age;
-char *owner;
-};
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
